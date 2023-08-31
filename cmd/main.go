e3bbdcdccc41760f219728f8042d0143c45a3c0a
echpochmak31 <echpochmak83@gmail.com>
2023-08-31 12:33:28 +0300

    feat: add application package

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,9 @@
 package main
 
 import (
-	"context"
-	"github.com/echpochmak31/avitotechbackendservice/internal/configs"
-	"github.com/echpochmak31/avitotechbackendservice/internal/controllers"
-	"github.com/echpochmak31/avitotechbackendservice/internal/repositories"
-	"github.com/echpochmak31/avitotechbackendservice/internal/services"
+	"github.com/echpochmak31/avitotechbackendservice/pkg/application"
 	"github.com/joho/godotenv"
 	"log"
-	"os"
 )
 
 func init() {
@@ -18,26 +13,9 @@ func init() {
 }
 
 func main() {
-	dbURL, err := configs.GetPostgresUrl()
+	err := application.RunSegmentsService("127.0.0.1:8080")
 	if err != nil {
 		log.Fatal(err)
 	}
-	fsConfig, err := configs.GetFileSystemConfig()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	repository, _ := repositories.NewPgxRepository(context.Background(), dbURL)
-	defer repository.Close()
-
-	segmentService := services.NewSegmentService(repository)
-	reportService := services.NewReportService(repository, fsConfig)
-	mc := controllers.InitMainController(segmentService, reportService, "127.0.0.1:8080")
-
-	err = mc.Run()
-
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer os.Exit(0)
+	//defer os.Exit(0)
 }
